85aa397dff1bd7ff5c128dc1444d0bd4aa6c3036
TitaRuiz <ggp2002es@gmail.com>
2024-01-22 20:03:03 +0100

    A las 20:00

--- src/main/java/com/corenetworks/repasoentidades/RepasoentidadesApplication.java
+++ src/main/java/com/corenetworks/repasoentidades/RepasoentidadesApplication.java
@@ -1,13 +1,21 @@
 package com.corenetworks.repasoentidades;
 
+import com.corenetworks.repasoentidades.servicio.ISocioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepasoentidadesApplication {
-
+public class RepasoentidadesApplication implements CommandLineRunner {
+	@Autowired
+    private ISocioServicio servicio;
 	public static void main(String[] args) {
 		SpringApplication.run(RepasoentidadesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(servicio.mostrarUno("12345678A"));
+	}
 }
